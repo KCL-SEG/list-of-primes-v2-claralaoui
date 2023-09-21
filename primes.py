c48f06fc7095fa7690bdab41f9934b3734d2d9ea
Clara <clara.laoui@kcl.ac.uk>
2023-09-21 21:41:03 +0200

    second try

--- primes.py
+++ primes.py
@@ -1,3 +1,14 @@
+def is_prime(n):
+    if n == 1:
+        return False
+    factors = 0
+    for i in range(1, n+1):
+        if n % i == 0:
+            factors += 1
+    if factors == 2:
+        return True
+    return False
+
 def primes(number_of_primes):
     if number_of_primes < 0:
         raise ValueError(f'The value must be positive, you entered {number_of_primes}')
