a718fc835d255789dd82407c203b04871ad43ea4
Clara <clara.laoui@kcl.ac.uk>
2023-09-21 21:39:21 +0200

    first try

--- primes.py
+++ primes.py
@@ -1,6 +1,11 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
+    if number_of_primes < 0:
+        raise ValueError(f'The value must be positive, you entered {number_of_primes}')
     list = []
+    next_number = 0
+    while len(list) < number_of_primes:
+        if is_prime(next_number):
+            list.append(next_number)
+        
+        next_number = next_number + 1
     return list
